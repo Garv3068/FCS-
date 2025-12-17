7198ef3360df4ae92a2a2781e81de2e7f343b8f7
Garv3068 <garv3068.beai25@chitkara.edu.in>
2025-12-17 14:43:33 +0530

    Create prog-1.py

---
+++ prog-1.py
@@ -0,0 +1,10 @@
+a=input().split()
+even=0
+odd=0
+for i in a:
+    if int(i)%2==0:
+        even+=1
+    else:
+        odd+=1
+print(f"Odd numbers in list: {odd}")
+print(f"Even numbers in list: {even}")
